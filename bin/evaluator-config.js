6145ea9fc1d7b4c5483173c90c4c440cb2fd8819
Christian G. Warden <cwarden@xerus.org>
2014-01-28 09:16:18 -0800

    Configure to start using foreman
    Configuration is read out of .env file.
    
    The collector is started on port 1080.
    The evaluator is started on port 1081.
    Both are started with node-supervisor.

--- bin/evaluator-config.js
+++ bin/evaluator-config.js
@@ -1,10 +1,15 @@
 // Default configuration for development.
+var mongo_url = process.env.MONGODB_URI;
+var params = /mongodb:\/\/(?:(\w+):(\w+)@)?([\w\.]+)(?::(\d+))?\/(\w+)/.exec(mongo_url);
+
 module.exports = {
-  "mongo-host": "127.0.0.1",
-  "mongo-port": 27017,
-  "mongo-database": "cube_development",
-  "mongo-username": null,
-  "mongo-password": null,
-  "http-port": 1081,
-  "authenticator": "allow_all"
+  "mongo-username": params[1],
+  "mongo-password": params[2],
+  "mongo-host": params[3],
+  "mongo-port": parseInt(params[4]),
+  "mongo-database": params[5],
+  "http-port": process.env.PORT,
+  "authenticator": process.env.EVALUATOR_AUTHENTICATOR,
+  "valid-email": process.env.VALID_EMAIL_DOMAIN,
+  "consumer-secret": process.env.CONSUMER_SECRET
 };
