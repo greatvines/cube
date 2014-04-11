b37cf9cf3a10bb472c7a69af901be0a5ce92b2ef
Christian G. Warden <cwarden@xerus.org>
2014-04-11 13:31:53 -0700

    Set mongo-url rather than individual connection parameters (LP #14365668)
    Fixes use of MONGODB_URI that contains multiple hosts in a replica set.

--- bin/evaluator-config.js
+++ bin/evaluator-config.js
@@ -1,13 +1,5 @@
-// Default configuration for development.
-var mongo_url = process.env.MONGODB_URI;
-var params = /mongodb:\/\/(?:(\w+):(\w+)@)?([\w\.]+)(?::(\d+))?\/(\w+)/.exec(mongo_url);
-
 module.exports = {
-  "mongo-username": params[1],
-  "mongo-password": params[2],
-  "mongo-host": params[3],
-  "mongo-port": parseInt(params[4]),
-  "mongo-database": params[5],
+  "mongo-url": process.env.MONGODB_URI,
   "http-port": process.env.PORT,
   "authenticator": process.env.EVALUATOR_AUTHENTICATOR,
   "valid-email": process.env.VALID_EMAIL_DOMAIN,
